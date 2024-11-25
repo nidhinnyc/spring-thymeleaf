f89345e5501fa3148eeff9e6cd1b33370adfc7b4
Nidhin Surendran <nidhins@gmail.com>
2024-11-25 22:59:30 +0530

    changes

--- src/main/java/com/hospitalmanagement/hospitalmanagement/controller/LoginController.java
+++ src/main/java/com/hospitalmanagement/hospitalmanagement/controller/LoginController.java
@@ -1,25 +1,30 @@
 package com.hospitalmanagement.hospitalmanagement.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
+
     @GetMapping("/login")
-    public String hospital(){
+    public String hospital() {
         return "login";
     }
+
     @PostMapping("/login")
-    public String showLoginPage(){
+    public String showLoginPage() {
         return "login";
     }
+
     @GetMapping("/contact")
-    public String contact(){
+    public String contact() {
         return "contact";
     }
+
     @PostMapping("/contact")
-    public String contact1(){
+    public String contact1() {
         return "contact";
     }
 }
--- src/main/java/com/hospitalmanagement/hospitalmanagement/controller/UserController.java
+++ src/main/java/com/hospitalmanagement/hospitalmanagement/controller/UserController.java
@@ -5,13 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import repository.UserRepository;
 import services.UserService;
 
 @Controller
 public class UserController {
-    @Autowired
-    private UserService Userservice;
-   @GetMapping("/") 
+   @Autowired
+   private UserService Userservice;
+
+   @Autowired
+   private UserRepository userRepository;
+
+   @GetMapping("/")
    public String viewHomePage(Model model) {
       model.addAttribute("listusers", Userservice.getAllUsers());
       return "usindex";
