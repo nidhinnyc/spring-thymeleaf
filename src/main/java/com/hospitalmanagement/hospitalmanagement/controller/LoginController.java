f4b15b45cbb1cfbb805226362e1b8f123130b273
Nidhin Surendran <nidhins@gmail.com>
2024-11-26 06:11:34 +0530

    changes

--- src/main/java/com/hospitalmanagement/hospitalmanagement/controller/LoginController.java
+++ src/main/java/com/hospitalmanagement/hospitalmanagement/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.hospitalmanagement.hospitalmanagement.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +9,7 @@ public class LoginController {
 
     @GetMapping("/login")
     public String hospital() {
+        System.out.println("loggin called");
         return "login";
     }
 
